e1c48eb055449c69af33f6221c49998cfa2c7595
Erkan EREN <github@erkaneren.dev>
2025-02-10 12:30:15 +0000

    devContainer QoL

--- dev.sh
+++
@@ -1,5 +0,0 @@
-rm -rf _site .jekyll-cache/ ; clear ; bundle exec jekyll serve --config "_devPatch.yml,_config.yml" --trace
-# init(
-#     git submodule update --init --recursive;
-#     bundle update
-# )
